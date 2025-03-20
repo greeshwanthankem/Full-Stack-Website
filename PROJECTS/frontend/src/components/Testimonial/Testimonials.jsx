26eae7e557c02f126af8a6d6922441110aca4c7d
greeshwanthankem <greeshwanthankem.com>
2025-03-20 11:36:13 +0530

    first commit

---
+++ PROJECTS/frontend/src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../assets/images/ava-1.jpg";
+import ava02 from "../../assets/images/ava-2.jpg";
+import ava03 from "../../assets/images/ava-3.jpg";
+
+const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+        <p>
+        TravelWorld is my go-to website for planning vacations. Their attention to detail and great deals make traveling so much easier. Our honeymoon was everything we hoped for, and more. Thank you, TravelWorld, for the memories of a lifetime!
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava01} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+        TravelWorld made my dream vacation to Europe a reality! 
+        The booking process was so easy, and the recommendations were tailored to my preferences.
+         From the flights to the cozy accommodations, everything was perfect. 
+         I’ll definitely use TravelWorld again for my next adventure!
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">Lia Franklin</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+        Thanks to TravelWorld, our family trip was stress-free and filled with wonderful experiences. The detailed itineraries and suggestions were spot-on, and the customer service was fantastic. We explored so much more than we ever thought we could!
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p>
+        I’ve used many travel websites before, but TravelWorld stands out. It’s so user-friendly, and the personalized trip ideas were exactly what I was looking for. I had an amazing solo trip to Bali, all thanks to them. Highly recommend!
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded-2" alt="" />
+          <div>
+            <h6 className="mb-0 mt-3">Lia Franklin</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default Testimonials;
