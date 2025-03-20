0442c0660d0d9aeb557da48314cc32fd44b5bcf6
ANKEM GREESHWANTH <151426443+greeshwanthankem@users.noreply.github.com>
2025-03-20 12:02:35 +0530

    Update index.js

--- PROJECTS/backend/index.js
+++ PROJECTS/backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "https://full-stack-website-bmrd.onrender.com",
+  origin: "https://full-stack-website-hazel.vercel.app",
 };
 
 // database connection
